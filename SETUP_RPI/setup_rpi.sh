c8ce1a9d6e1711168f5abdecc33d522ee92796e7
Michele Carenini <84254228+Z4nna@users.noreply.github.com>
2024-12-02 17:12:15 +0100

    Update setup_rpi.sh
    Fixed some path issues, added better colors. Tested the script

--- SETUP_RPI/setup_rpi.sh
+++ SETUP_RPI/setup_rpi.sh
@@ -3,60 +3,71 @@ sudo apt-get upgrade -y
 
 RED='\033[0;31m'
 NC='\033[0m' # No Color
+GREEN='\033[0;32m'        # Green
+YELLOW='\033[0;33m'       # Yellow
 # current working directory
 cwd=$(pwd)
 
+echo "Installing Git..."
+sudo apt-get install git -y
+echo "${GREEN}Done.${NC}"
+
+echo "Installing Vim..."
+sudo apt-get install vim -y
+echo "${GREEN}Done.${NC}"
+
 echo "Installing Midnight Commander..."
 sudo apt-get install mc -y
-echo "Done."
+echo "${GREEN}Done.${NC}"
 
 echo "Installing Ranger..."
 sudo apt-get install ranger -y
-echo "Done."
+echo "${GREEN}Done.${NC}"
 
 echo "Installing fzf..."
 sudo apt-get install fzf -y
-echo "Done."
+echo "${GREEN}Done.${NC}"
 
 echo "Installing htop..."
 sudo apt-get install htop -y
-echo "Done."
+echo "${GREEN}Done.${NC}"
 
 echo "Installing thefu*k..."
 sudo apt-get install thefuck -y
-echo "Done."
+echo "${GREEN}Done.${NC}"
 
 echo "Installing tmux..."
 sudo apt-get install tmux -y
-echo "Done."
+echo "${GREEN}Done.${NC}"
 
 echo "Configuring tmux..."
 
 echo "Installing tmux tpm..."
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-echo "Done."
-
-mkdir ~/.config/tmux
+echo "${GREEN}Done.${NC}"
+echo "${YELLOW}"
+mkdir -p ~/.config/tmux
 cp ./tmux.conf ~/.config/tmux/
 
 ~/.tmux/plugins/tpm/bin/install_plugins
 ~/.tmux/plugins/tpm/bin/update_plugins all
-echo "Done."
+echo "${NC}
+echo "${GREEN}Done.${NC}"
 
 echo "Installing neovim..."
 echo "Installing dependencies..."
-sudo apt-get install ninja-build gettext cmake unzip curl build-essential
-
-git clone https://github.com/neovim/neovim ~/
+sudo apt-get install ninja-build gettext cmake unzip curl build-essential -y
+echo "${YELLOW}"
+git clone https://github.com/neovim/neovim.git ~/neovim
 cd ~/neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
 
-echo "Done."
+echo "${GREEN}Done.${NC}"
 
 echo "Installing nvchad..."
 git clone https://github.com/NvChad/starter ~/.config/nvim
 echo -e "${RED}Done! Run :MasonInstallAll after nvchad is done installing."
-echo -e "${RED}Then delete ~/.config/nvim/.git"
+echo -e "Then delete ~/.config/nvim/.git${NC}"
 
 echo "Installing ROS2 Humble..."
 
@@ -66,12 +77,13 @@ sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
-sudo apt upgrade
-sudo apt install ros-humble-ros-base
-sudo apt install ros-dev-tools
+sudo apt upgrade -y
+sudo apt install ros-humble-ros-base -y
+sudo apt install ros-dev-tools -y
 echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+source ~/.bashrc
 
-echo "Done."
+echo "${GREEN}Done.${NC}"
 
 echo "alias rg="ranger "" >> ~/.bashrc
 
@@ -79,4 +91,4 @@ echo "Building Nereo ROS2 packages..."
 cd $(cwd)/../ros2_ws
 colcon build
 
-echo "All set up!"
+echo "${RED}All set up!${NC}"
